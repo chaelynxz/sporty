f170e1640a215169a87aefc0f42722506d8c0524
chaelynxz <chaelynxzbetsayda@gmail.com>
2024-12-05 20:06:57 +0800

    final design basketball

--- basketball.js
+++ basketball.js
@@ -1,4 +1,136 @@
-// const basketball = document.querySelector(".basketball");
-//  function activeBasketball() {
-//     basketball.display.style.backgroundColor = "#ffff00"
-//  }
+document.querySelector(".btn-1").addEventListener("click", function () {
+  var contentPG = document.getElementById("circlePG");
+  var contentSG = document.getElementById("circleSG");
+  var contentSF = document.getElementById("circleSF");
+  var contentPF = document.getElementById("circlePF");
+  var contentC = document.getElementById("circleC");
+  var paragraph = document.getElementById("defCon");
+  console.log("hit button 1");
+  if (contentPG.classList.contains("hidden")) {
+    contentPG.classList.remove("hidden");
+    contentPG.classList.add("visible");
+    paragraph.textContent = "The playmaker who handles the ball and sets up plays.";
+    contentSG.classList.remove("visible");
+    contentSG.classList.add("hidden");
+    contentSF.classList.remove("visible");
+    contentSF.classList.add("hidden");
+    contentPF.classList.remove("visible");
+    contentPF.classList.add("hidden");
+    contentC.classList.remove("visible");
+    contentC.classList.add("hidden");
+  } else {
+    contentPG.classList.remove("visible");
+    contentPG.classList.add("hidden");
+    paragraph.textContent = "";
+  }
+});
+document.querySelector(".btn-2").addEventListener("click", function () {
+  var contentSG = document.getElementById("circleSG");
+  var contentPG = document.getElementById("circlePG");
+  var contentSF = document.getElementById("circleSF");
+  var contentPF = document.getElementById("circlePF");
+  var contentC = document.getElementById("circleC");
+  var paragraph = document.getElementById("defCon");
+  console.log("hit button 2");
+  if (contentSG.classList.contains("hidden")) {
+    contentSG.classList.remove("hidden");
+    contentSG.classList.add("visible");
+    paragraph.textContent = "A strong scorer, often focusing on outside shooting.";
+    contentPG.classList.remove("visible");
+    contentPG.classList.add("hidden");
+    contentSF.classList.remove("visible");
+    contentSF.classList.add("hidden");
+    contentPF.classList.remove("visible");
+    contentPF.classList.add("hidden");
+    contentC.classList.remove("visible");
+    contentC.classList.add("hidden");
+
+  } else {
+    contentSG.classList.remove("visible");
+    contentSG.classList.add("hidden");
+    paragraph.textContent = "";
+  }
+});
+document.querySelector(".btn-3").addEventListener("click", function () {
+  var contentSG = document.getElementById("circleSG");
+  var contentPG = document.getElementById("circlePG");
+  var contentSF = document.getElementById("circleSF");
+  var contentPF = document.getElementById("circlePF");
+  var contentC = document.getElementById("circleC");
+  var paragraph = document.getElementById("defCon");
+  console.log("hit button 3");
+  if (contentSF.classList.contains("hidden")) {
+    contentSF.classList.remove("hidden");
+    contentSF.classList.add("visible");
+    paragraph.textContent = "A versatile player who can score, defend, and rebound.";
+    contentPG.classList.remove("visible");
+    contentPG.classList.add("hidden");
+    contentSG.classList.remove("visible");
+    contentSG.classList.add("hidden");
+    contentPF.classList.remove("visible");
+    contentPF.classList.add("hidden");
+    contentC.classList.remove("visible");
+    contentC.classList.add("hidden");
+
+  } else {
+    contentSF.classList.remove("visible");
+    contentSF.classList.add("hidden");
+    paragraph.textContent = "";
+  }
+});
+document.querySelector(".btn-4").addEventListener("click", function () {
+  var contentSG = document.getElementById("circleSG");
+  var contentPG = document.getElementById("circlePG");
+  var contentSF = document.getElementById("circleSF");
+  var contentPF = document.getElementById("circlePF");
+  var contentC = document.getElementById("circleC");
+  var paragraph = document.getElementById("defCon");
+  console.log("hit button 4");
+  if (contentPF.classList.contains("hidden")) {
+    contentPF.classList.remove("hidden");
+    contentPF.classList.add("visible");
+    paragraph.textContent = "A strong player who operates near the basket for scoring and rebounding. ";
+    contentPG.classList.remove("visible");
+    contentPG.classList.add("hidden");
+    contentSG.classList.remove("visible");
+    contentSG.classList.add("hidden");
+    contentSF.classList.remove("visible");
+    contentSF.classList.add("hidden");
+    contentC.classList.remove("visible");
+    contentC.classList.add("hidden");
+
+  } else {
+    contentPF.classList.remove("visible");
+    contentPF.classList.add("hidden");
+    paragraph.textContent = "";
+  }
+});
+
+document.querySelector(".btn-5").addEventListener("click", function () {
+    var contentSG = document.getElementById("circleSG");
+    var contentPG = document.getElementById("circlePG");
+    var contentSF = document.getElementById("circleSF");
+    var contentPF = document.getElementById("circlePF");
+    var contentC = document.getElementById("circleC");
+    var paragraph = document.getElementById("defCon");
+    console.log("hit button 5");
+    if (contentC.classList.contains("hidden")) {
+      contentC.classList.remove("hidden");
+      contentC.classList.add("visible");
+      paragraph.textContent = "The tallest player, responsible for rebounding, blocking shots, and scoring close to the hoop.";
+      contentPG.classList.remove("visible");
+      contentPG.classList.add("hidden");
+      contentSG.classList.remove("visible");
+      contentSG.classList.add("hidden");
+      contentSF.classList.remove("visible");
+      contentSF.classList.add("hidden");
+      contentPF.classList.remove("visible");
+      contentPF.classList.add("hidden");
+
+    } else {
+      contentC.classList.remove("visible");
+      contentC.classList.add("hidden");
+      paragraph.textContent = "";
+    }
+  });
+  
